bc8b3450650d08e59977891c8403a222d2fc490d
shixiaofei <gabe.shi@icloud.com>
2025-01-05 14:57:39 +0800

    更新生产环境API路径，调整注册页面背景样式，修改构建路径，优化侧边栏Logo组件样式

--- src/views/register.vue
+++ src/views/register.vue
@@ -128,7 +128,6 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
   background-size: cover;
 }
 
