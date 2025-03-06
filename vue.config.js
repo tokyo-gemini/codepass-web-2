a5b491f232e53a892bc57cf77a0ebe646e746b39
shixiaofei <gabe.shi@icloud.com>
2025-03-06 16:01:55 +0800

    优化 vue.config.js 配置，调整代码格式，更新 API 目标地址

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-'use strict';
-const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
+'use strict'
+const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 function resolve(dir) {
-  return path.join(__dirname, dir);
+  return path.join(__dirname, dir)
 }
 
-const name = process.env.VUE_APP_TITLE || '码上通管理系统';
-const port = process.env.port || process.env.npm_config_port || 80;
+const name = process.env.VUE_APP_TITLE || '码上通管理系统'
+const port = process.env.port || process.env.npm_config_port || 80
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
@@ -33,38 +33,38 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://42848337.r18.vip.cpolar.cn`,
+        target: `http://192.168.2.17:9876`,
         changeOrigin: true,
         pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: '',
-        },
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
       },
       [process.env.VUE_APP_BASE_UPLOAD]: {
-        target: `http://42848337.r18.vip.cpolar.cn`,
+        target: `http://192.168.2.17:9876`,
         changeOrigin: true,
         pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_UPLOAD]: '',
-        },
-      },
+          ['^' + process.env.VUE_APP_BASE_UPLOAD]: ''
+        }
+      }
     },
-    disableHostCheck: true,
+    disableHostCheck: true
   },
   css: {
     loaderOptions: {
       sass: {
-        sassOptions: { outputStyle: 'expanded' },
+        sassOptions: { outputStyle: 'expanded' }
       },
       postcss: {
-        plugins: [require('tailwindcss'), require('autoprefixer')],
-      },
-    },
+        plugins: [require('tailwindcss'), require('autoprefixer')]
+      }
+    }
   },
   configureWebpack: {
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src'),
-      },
+        '@': resolve('src')
+      }
     },
     plugins: [
       // http://doc.ruoyi.vip/ruoyi-vue/other/faq.html#使用gzip解压缩静态文件
@@ -74,16 +74,16 @@ module.exports = {
         filename: '[path][base].gz[query]', // 压缩后的文件名
         algorithm: 'gzip', // 使用gzip压缩
         minRatio: 0.8, // 压缩比例，小于 80% 的文件不会被压缩
-        deleteOriginalAssets: false, // 压缩后删除原文件
-      }),
-    ],
+        deleteOriginalAssets: false // 压缩后删除原文件
+      })
+    ]
   },
   chainWebpack(config) {
-    config.plugins.delete('preload'); // TODO: need test
-    config.plugins.delete('prefetch'); // TODO: need test
+    config.plugins.delete('preload') // TODO: need test
+    config.plugins.delete('prefetch') // TODO: need test
 
     // set svg-sprite-loader
-    config.module.rule('svg').exclude.add(resolve('src/assets/icons')).end();
+    config.module.rule('svg').exclude.add(resolve('src/assets/icons')).end()
     config.module
       .rule('icons')
       .test(/\.svg$/)
@@ -92,9 +92,9 @@ module.exports = {
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
       .options({
-        symbolId: 'icon-[name]',
+        symbolId: 'icon-[name]'
       })
-      .end();
+      .end()
 
     config.when(process.env.NODE_ENV !== 'development', (config) => {
       config
@@ -103,10 +103,10 @@ module.exports = {
         .use('script-ext-html-webpack-plugin', [
           {
             // `runtime` must same as runtimeChunk name. default is `runtime`
-            inline: /runtime\..*\.js$/,
-          },
+            inline: /runtime\..*\.js$/
+          }
         ])
-        .end();
+        .end()
 
       config.optimization.splitChunks({
         chunks: 'all',
@@ -115,23 +115,23 @@ module.exports = {
             name: 'chunk-libs',
             test: /[\\/]node_modules[\\/]/,
             priority: 10,
-            chunks: 'initial', // only package third parties that are initially dependent
+            chunks: 'initial' // only package third parties that are initially dependent
           },
           elementUI: {
             name: 'chunk-elementUI', // split elementUI into a single package
             test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
-            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            priority: 20 // the weight needs to be larger than libs and app or it will be packaged into libs or app
           },
           commons: {
             name: 'chunk-commons',
             test: resolve('src/components'), // can customize your rules
             minChunks: 3, //  minimum common number
             priority: 5,
-            reuseExistingChunk: true,
-          },
-        },
-      });
-      config.optimization.runtimeChunk('single');
-    });
-  },
-};
+            reuseExistingChunk: true
+          }
+        }
+      })
+      config.optimization.runtimeChunk('single')
+    })
+  }
+}
